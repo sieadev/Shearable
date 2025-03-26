45340207d3a531a5307203bb72a35fb307497fcf
SIEA <69807609+DrVosss@users.noreply.github.com>
2025-03-26 17:41:05 +0100

    dont work anyways

--- src/main/java/siea/dev/shearable/Shearable.java
+++ src/main/java/siea/dev/shearable/Shearable.java
@@ -12,8 +12,6 @@ import siea.dev.shearable.registry.ShearableRegistry;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class Shearable extends JavaPlugin {
 
@@ -30,16 +28,10 @@ public final class Shearable extends JavaPlugin {
     @Override
     public void onDisable() {
         ConfigurationSection section = getConfig().createSection("plants");
-        Set<String> configKeys = section.getKeys(false);
-        Set<String> registryKeys = ShearableRegistry.getRegistry().keySet().stream()
-                .map(Material::name)
-                .collect(Collectors.toSet());
-        if (!configKeys.equals(registryKeys)) {
-            for (Map.Entry<Material, DropState> entry : ShearableRegistry.getRegistry().entrySet()) {
-                section.set(entry.getKey().name(), entry.getValue().name());
-            }
-            saveConfig();
+        for (Map.Entry<Material, DropState> entry : ShearableRegistry.getRegistry().entrySet()) {
+            section.set(entry.getKey().name(), entry.getValue().name());
         }
+        saveConfig();
     }
 
     public static void log(String message) {
